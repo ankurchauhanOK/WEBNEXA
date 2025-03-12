9d5eba281ecbab7c0e3c3c9282dfe7b1f5b94a2d
Ankur Chauhan <115866837+ankurchauhanOK@users.noreply.github.com>
2025-03-12 06:39:49 +0000

    logo

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
     });
   }, []);
 
-  useDocTitle("MLD | Molad e Konsult - Bespoke Web and Mobile Applications");
+  useDocTitle("WebNexa- Bespoke Web and Mobile Applications");
 
   return (
     <>
